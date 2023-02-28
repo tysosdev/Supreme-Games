1522d101e5a0c9fecb58d582e9cfae33ea85e652
SamMerlin <>
2023-02-28 17:39:27 -0500

    fixed back button maby

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if(window.self == window.top){
     var a = window.open("about:blank", "_blank");
-    a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin: 0;}</style><body><iframe style="height:100%; width:100%; top:0px; left:0px; position:absolute;  z-index:1;" src="' + window.location.href + '" frameborder="0"></iframe></body></html>';
+    a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin: 0;}</style><script>window.onpopstate = function() {window.location.replace("https://www.studymath.ml/")}; history.pushState({}, "");</script><body><iframe style="height:100%; width:100%; top:0px; left:0px; position:absolute;  z-index:1;" src="' + window.location.href + '" frameborder="0"></iframe></body></html>';
     if(a == null){
         document.innerHTML = "allow pop ups"
     }else{
