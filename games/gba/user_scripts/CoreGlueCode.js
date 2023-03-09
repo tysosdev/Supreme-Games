5a42f8e69dedb3f1c8a8b3111404a258189a48a0
MastrCheef99 <120070513+MastrCheef99@users.noreply.github.com>
2023-03-08 20:56:06 -0500

    finally games

--- games/gba/user_scripts/CoreGlueCode.js
+++ games/gba/user_scripts/CoreGlueCode.js
@@ -9,6 +9,11 @@
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 var games = {
+    "harvestmoon":"Harvest Moon: Friends of Mineral Town",
+    "mariotennis":"Mario Tennis: Power Tour",
+    "mariogolf":"Mario Golf: Advance Tour",
+    "zeromission":"Metroid: Zero Mission",
+    "MariovDonkey":"Mario vs Donkey Kong",
     "dkc1":"Donkey Kong Country 1",
     "fire_emblem_the_binding_blade":"FireEmblem - The Binding Blade",
     "castlevania":"Castlevania",
