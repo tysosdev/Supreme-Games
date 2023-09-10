0edea454cfc0f7582988ff75ea50efe18330844b
Blue222 <57820015+supremegmes@users.noreply.github.com>
2023-09-10 11:54:40 -0400

    actually for real fullscreen whg and circloO

--- games/worldsHardestGame/canvas.js
+++ games/worldsHardestGame/canvas.js
@@ -2,6 +2,6 @@ if (mobile) {
     var canvasElement = $("<canvas oncontextmenu='return false;' id='twhgCanvas'></canvas>").get(0);
     var canvas = canvasElement.getContext("2d");
 } else {
-    var canvasElement = $("<canvas id='twhgCanvas' width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>").get(0);
+    var canvasElement = $("<canvas id='twhgCanvas' width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "' style='width: calc((100vh*4)/3); height: 100vh;'></canvas>").get(0);
     var canvas = canvasElement.getContext("2d");
 }
