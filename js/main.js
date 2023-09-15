5d3598d08a9f8246a59b8e5c322aa1064a337e73
Blue222 <57820015+supremegmes@users.noreply.github.com>
2023-09-15 18:20:34 -0400

    add secret localstorage option for firefox compatibility

--- js/main.js
+++ js/main.js
@@ -7,11 +7,19 @@ var api = "https://api." + location.hostname + ":3000";
 var socket;
 var searchSuggestionFocus = false;
 if(window.self == window.top){
+
     var a = window.open("about:blank", "_blank");
+    if(localStorage.getItem("ff" == "true")){
+        a.addEventListener("DOMContentLoaded", function () {
+            a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin: 0;}</style><body onload=' + "'" + 'document.getElementsByTagName("iframe")[0].focus();' + "'" + '><iframe style="height:100%; width:100%; top:0px; left:0; position:absolute;  z-index:1;" src="https://' + window.location.hostname + '" frameborder="0"></iframe></body></html>';
+            window.location.replace("https://classroom.google.com/");
+        });
+    }else{
     if(a != null){
         a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin: 0;}</style><body onload=' + "'" + 'document.getElementsByTagName("iframe")[0].focus();' + "'" + '><iframe style="height:100%; width:100%; top:0px; left:0; position:absolute;  z-index:1;" src="https://' + window.location.hostname + '" frameborder="0"></iframe></body></html>';
         window.location.replace("https://classroom.google.com/");
     }
+}
 }
  //show discord popup
  if (localStorage.getItem("discord") != "stop") {
@@ -111,14 +119,29 @@ function opendiscrod() {
     window.location.replace("https://discord.gg/xYSgcdDXrJ")
 }
 async function abGame(gameid) {
+    
         var a = window.open("about:blank", "_blank");
+        if(localStorage.getItem("ff" == "true")){
+            a.addEventListener("DOMContentLoaded", function () {
+                a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body,html{margin:0;overflow:hidden}</style><body onload=' + "'" + 'document.getElementsByTagName("iframe")[0].focus();' + "'" + '><iframe style="height:100%;width:100%;border:0;top:0px;left:0;position:absolute;z-index:1;overflow:hidden" src="https://' + window.location.hostname + '/games/' + gameid + '/index.html" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-downloads allow-same-origin" loading="eager" allowfullscreen allowpaymentrequest></iframe></body></html>';
+                socket.emit('game', gameid);    
+            });
+        }else{
         a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body,html{margin:0;overflow:hidden}</style><body onload=' + "'" + 'document.getElementsByTagName("iframe")[0].focus();' + "'" + '><iframe style="height:100%;width:100%;border:0;top:0px;left:0;position:absolute;z-index:1;overflow:hidden" src="https://' + window.location.hostname + '/games/' + gameid + '/index.html" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-downloads allow-same-origin" loading="eager" allowfullscreen allowpaymentrequest></iframe></body></html>';
         socket.emit('game', gameid);    
+        }
 }
 async function abFlash(gameid) {
     var a = window.open("about:blank", "_blank");
+    if(localStorage.getItem("ff" == "true")){
+        a.addEventListener("DOMContentLoaded", function () {
+            a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body,html{margin:0;overflow:hidden}</style><body onload=' + "'" + 'document.getElementsByTagName("iframe")[0].focus();' + "'" + '><iframe style="height:100%;width:100%;border:0;top:0px;left:0;position:absolute;z-index:1;overflow:hidden" src="https://' + window.location.hostname + '/dfl/?f=' + gameid + '" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-downloads allow-same-origin" loading="eager" allowfullscreen allowpaymentrequest></iframe></body></html>';
+            socket.emit('game', gameid);
+        });
+    }else{
     a.document.documentElement.innerHTML = '<!DOCTYPE html><html><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body,html{margin:0;overflow:hidden}</style><body onload=' + "'" + 'document.getElementsByTagName("iframe")[0].focus();' + "'" + '><iframe style="height:100%;width:100%;border:0;top:0px;left:0;position:absolute;z-index:1;overflow:hidden" src="https://' + window.location.hostname + '/dfl/?f=' + gameid + '" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-downloads allow-same-origin" loading="eager" allowfullscreen allowpaymentrequest></iframe></body></html>';
-    socket.emit('game', gameid);    
+    socket.emit('game', gameid);  
+    }  
 }
 function addgame(sectionid, gameid, gamename, img, properties, func) {
     document.getElementById(sectionid).innerHTML += '<div class="game" onclick="' + func + '(' + "'" + gameid + "'" + ')"><img class="gimg" alt="' + gamename + '" ' + properties + 'src="/images/' + img + '"><div class="gradient"></div><h class="text">' + gamename + '</h></div>'
