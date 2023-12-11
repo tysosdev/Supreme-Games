2b324243c8be38e3cf37bc260575d1be8f57333e
Samuel Merlin <57820015+supremegme@users.noreply.github.com>
2023-12-10 19:02:09 -0500

    fix the backround blob

--- sg/js/blob.js
+++ sg/js/blob.js
@@ -3,7 +3,7 @@ const blob = document.getElementById("blob");
 document.body.onpointermove = event => {
     const {clientX, clientY} = event;
     blob.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
+        left: clientX + "px",
+        top: clientY + document.getElementById("html").scrollTop + "px"
     }, {duration:3000, fill:"forwards"});
 }
